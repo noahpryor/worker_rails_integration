0c41348a0c8a3eef64984d8583c90cde7c3c2c59
thousandsofthem <thousandsofthem@gmail.com>
2013-02-09 05:12:30 +0300

    Initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorkerRailsIntegration::Application.config.secret_token = 'b7547df363e53262832af29cd944ae2d8354adb069e10e309482c67ddcc3b073260fe603d9ccc2c35ef203ffeb09a56593adb87c307e474f50d71caab48e1ccf'
